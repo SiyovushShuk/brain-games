a96d033d657bf487899b896118a27e440958fcca
Seva <shukurovsiyovush1@gmail.com>
2026-01-22 12:59:34 +0500

    Add in progression game question and answer

--- brain_games_package/games/brain-progression.py
+++ brain_games_package/games/brain-progression.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import List
+from typing import List, Tuple
 
 
 
@@ -20,3 +20,20 @@ def do_random_arithmetic_row() -> List[int]:
         start_position += step_progression
 
     return progression_row
+
+
+def do_question_answer(progression_row: List[int]) -> Tuple[str, str]:
+
+    answer_position = randint(0, len(progression_row) - 1)
+    answer = progression_row[answer_position]
+
+    progression_row[answer_position] = '..'
+    question = ' '.join(map(str, progression_row))
+
+    return(answer, question)
+
+
+
+row = do_random_arithmetic_row()
+print(row)
+print(do_question_answer(row))
