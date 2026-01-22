eefff936e5cd4c9f711e432c20bed2f0a9e625b1
Seva <shukurovsiyovush1@gmail.com>
2026-01-22 11:28:14 +0500

    Add title in main.py

--- brain_games_package/scripts/main.py
+++ brain_games_package/scripts/main.py
@@ -1,7 +1,8 @@
 
 
 def main():
-    print('Hello world!')
+    username = input('Welcome to the Brain Games!\nMay I have your name? ')
+    print(f'Hello, {username}')
 
 if __name__ == "__main__":
     main()
