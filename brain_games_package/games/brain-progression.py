47a2fa115d8fe25ef6c88c5e02dd39e42dc63627
Seva <shukurovsiyovush1@gmail.com>
2026-01-22 12:56:13 +0500

    Add arithmetic progression game

---
+++ brain_games_package/games/brain-progression.py
@@ -0,0 +1,22 @@
+from random import randint
+from typing import List, Tuple
+
+
+
+
+def do_random_arithmetic_row() -> List[int]:
+
+    step_progression = randint(-15, 15)
+
+
+    start_position = step_progression * randint(-4, 4)
+
+    progression_row: List[int] = []
+
+    for i in range(10):
+
+        progression_row.append(start_position)
+
+        start_position += step_progression
+
+    return progression_row
